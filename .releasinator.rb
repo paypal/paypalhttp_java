06bd1414dcd78d6f13dbfee6d04cdb483e517a8f
sashaweiss and sdcoffey <steve.coffey@getbraintree.com>
2017-08-16 14:56:18 -0700

    Update releasinator config

--- .releasinator.rb
+++ .releasinator.rb
@@ -6,25 +6,29 @@ configatron.product_name = "BraintreeHttp Java"
 configatron.prerelease_checklist_items = [
 ]
 
-# The directory where all distributed docs are.  If not specified, the default is `.`.
-# configatron.base_docs_dir = '.'
+# Other tasks
+def build
+  CommandProcess.command("./gradlew clean build test")
+end
 
 configatron.build_method = method(:build)
 
+def validate_version_match()
+  if 'v'+package_version != @current_release.version
+    Printer.fail("package version #{package_version} does not match changelog version #{@current_release.version}.")
+    abort()
+  end
+	Printer.success("package version #{package_version} matches latest changelog version #{@current_release.version}.")
+end
+
 def publish_to_package_manager(version)
-  abort("please implement publish_to_package_manager method")
-  # task :release_braintreehttp do
-  #   sh "./gradlew :braintreehttp:uploadArchives  :braintreehttp-testutils:uploadArchives"
-  #   sh "./gradlew :braintreehttp:closeRepository"
-  #
-  #   puts "Sleeping for one minute to allow BraintreeHttp modules to close"
-  #   sleep 60
-  #   sh "./gradlew :braintreehttp:promoteRepository :braintreehttp-testutils:promoteRepository"
-  #
-  #   puts "Sleeping for ten minutes to allow BraintreeHttp modules to be promoted"
-  #   sleep 600
-  #   puts "Braintreehttp has been released"
-  # end
+  puts "Version passed to method: #{version}"
+  puts "Package Version: #{package_version}"
+  # CommandProcessor.command("./gradlew uploadArchives", live_output=true)
+  # CommandProcessor.command("./gradlew closeRepository", live_output=true)
+  # CommandProcessor.command("sleep 60")
+  # puts "Sleeping for one minute to allow BraintreeHttp modules to close"
+  # CommandProcessory.command("./gradlew promoteRepository", live_output=true)
 end
 
 # The method that publishes the project to the package manager.  Required.
@@ -32,25 +36,32 @@ configatron.publish_to_package_manager_method = method(:publish_to_package_manag
 
 
 def wait_for_package_manager(version)
-  puts "I'm here!"
+  CommandProcessor.wait_for("wget -U \"non-empty-user-agent\" -qO- http://central.maven.org/maven2/com/braintreepayments/braintreehttp/1.2.6/braintreehttp-#{version}.pom | cat")
 end
 
 # The method that waits for the package manager to be done.  Required.
 configatron.wait_for_package_manager_method = method(:wait_for_package_manager)
 
 # True if publishing the root repo to GitHub.  Required.
-configatron.release_to_github = false
+configatron.release_to_github = true
 
-
-# Other tasks
-def build
-  CommandProcess.command("./gradlew clean build test")
-end
 
 def test
   CommandProcess.command("./gradlew clean test")
 end
 
+def package_version
+	File.open("build.gradle", 'r') do |f|
+		f.each_line do |line|
+			puts line
+			if line.match (/version \'\d+.\d+.\d+'/)
+				return line.strip.split('\'')[1]
+			end
+		end
+	end
+end
+
 task :test do
   test
 end
+
--- Rakefile
+++ Rakefile
@@ -3,27 +3,3 @@ require 'rake'
 spec = Gem::Specification.find_by_name 'releasinator'
 load "#{spec.gem_dir}/lib/tasks/releasinator.rake"
 
-task :default => :test
-
-task :build do
-  sh "./gradlew clean build"
-end
-
-task :test do
-  sh "./gradlew clean test"
-end
-
-task :release_braintreehttp do
-  sh "./gradlew :braintreehttp:uploadArchives  :braintreehttp-testutils:uploadArchives"
-  sh "./gradlew :braintreehttp:closeRepository"
-
-  puts "Sleeping for one minute to allow BraintreeHttp modules to close"
-  sleep 60
-  sh "./gradlew :braintreehttp:promoteRepository :braintreehttp-testutils:promoteRepository"
-
-  puts "Sleeping for ten minutes to allow BraintreeHttp modules to be promoted"
-  sleep 600
-  puts "Braintreehttp has been released"
-end
-
-
