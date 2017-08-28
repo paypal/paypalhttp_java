456187052a1e1cda0cd53307a3e434d62d8802f7
Sasha Weiss <sasha.weiss@getbraintree.com>
2017-08-28 23:35:59 +0000

    Update .releasinator.rb for automated release

--- .releasinator.rb
+++ .releasinator.rb
@@ -2,20 +2,50 @@ task :default => :test
 
 configatron.product_name = "BraintreeHttp Java"
 
-# Other tasks
-def build
-  CommandProcessor.command("./gradlew clean build test")
+# Custom validations
+def package_version
+	File.open("build.gradle", 'r') do |f|
+		f.each_line do |line|
+			if line.match (/version \'\d+.\d+.\d+'/)
+				return line.strip.split('\'')[1]
+			end
+		end
+	end
 end
 
-configatron.build_method = method(:build)
-
-def validate_version_match()
+def validate_version_match
   if package_version != @current_release.version
     Printer.fail("package version #{package_version} does not match changelog version #{@current_release.version}.")
     abort()
   end
 	Printer.success("package version #{package_version} matches latest changelog version #{@current_release.version}.")
 end
+
+def test
+  CommandProcessor.command("./gradlew clean build test")
+end
+
+configatron.custom_validation_methods = [
+  method(:validate_version_match),
+  method(:test)
+]
+
+# Update version, build, and publish to Maven
+def update_version_method(version, semver_type)
+  contents = File.read("build.gradle")
+  contents = contents.gsub(/version '\d+.\d+.\d+'/, "version '#{version}'")
+  File.open("build.gradle", "w") do |file|
+    file.puts contents
+  end
+end
+
+configatron.update_version_method = method(:update_version_method)
+
+def build
+  CommandProcessor.command("./gradlew clean build")
+end
+
+configatron.build_method = method(:build)
 
 def publish_to_package_manager(version)
   CommandProcessor.command("./gradlew braintreehttp:uploadArchives", live_output=true)
@@ -28,43 +58,14 @@ def publish_to_package_manager(version)
   CommandProcessor.command("./gradlew braintreehttp-testutils:closeAndReleaseRepository", live_output=true)
 end
 
-# The method that publishes the project to the package manager.  Required.
 configatron.publish_to_package_manager_method = method(:publish_to_package_manager)
-
 
 def wait_for_package_manager(version)
   CommandProcessor.wait_for("wget -U \"non-empty-user-agent\" -qO- http://central.maven.org/maven2/com/braintreepayments/braintreehttp/#{version}/braintreehttp-#{version}.pom | cat")
 end
 
-# The method that waits for the package manager to be done.  Required.
 configatron.wait_for_package_manager_method = method(:wait_for_package_manager)
 
-# True if publishing the root repo to GitHub.  Required.
+# Miscellania
 configatron.release_to_github = true
-
-
-# List of items to confirm from the person releasing.  Required, but empty list is ok.
 configatron.prerelease_checklist_items = []
-configatron.custom_validation_methods = [
-  method(:validate_version_match)
-]
-
-def test
-  CommandProcess.command("./gradlew clean test")
-end
-
-def package_version
-	File.open("build.gradle", 'r') do |f|
-		f.each_line do |line|
-			puts line
-			if line.match (/version \'\d+.\d+.\d+'/)
-				return line.strip.split('\'')[1]
-			end
-		end
-	end
-end
-
-task :test do
-  test
-end
-
--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source "https://rubygems.org"
 ruby "2.4"
 
 gem "rake"
-gem "releasinator", "0.6.4"
+gem "releasinator", :git => "git@github.com:paypal/releasinator.git"
